9fa19b6549065986b11e3afd79f0bc70feee6b38
Miyato1771 <sakuradamiyato@gmail.com>
2026-07-28 13:36:21 +0900

    apiインクルードしてないので修正

--- web_server.py
+++ web_server.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.templating import Jinja2Templates
 
-# 1. auth_router のインポートを追加
+# 1. 各ルーティングのインポート (deploy_router を追加)
 from routes.alarm_api import router as alarm_router
 from routes.auth_api import router as auth_router
+from routes.deploy_api import router as deploy_router  # 🔑 追加！
 from routes.moderation_api import router as moderation_router
 from routes.ui import router as ui_router
 from routes.vc_api import router as vc_router
@@ -19,12 +20,13 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.state.templates = templates
 
-# 2. auth_router をアプリに登録
+# 2. 各ルーターをアプリに登録
 app.include_router(ui_router)
-app.include_router(auth_router)  # ← 追加しました！
+app.include_router(auth_router)
 app.include_router(alarm_router)
 app.include_router(vc_router)
 app.include_router(moderation_router)
+app.include_router(deploy_router)  # 🔑 追加！(これで /api/deploy/github が開通します)
 
 PORT = int(os.getenv("PORT", 8000))
 
